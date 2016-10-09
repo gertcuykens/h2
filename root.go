feb0585b7a6490157716194db017b9227fd7d26e
Gert Cuykens <gert.cuykens@gmail.com>
2016-10-09 11:24:26 +0200

    root

--- root.go
+++ root.go
@@ -8,12 +8,11 @@ import (
 // RootHTTP Handler
 func RootHTTP(root string, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := os.Open(root + r.URL.Path)
-		if err != nil {
+		if _, err := os.Stat(root + r.URL.Path); os.IsNotExist(err) {
 			h.ServeHTTP(w, r)
 			return
 		}
-		r.URL.Path = root + r.URL.Path
-		h.ServeHTTP(w, r)
+		h2 := http.FileServer(http.Dir(root))
+		h2.ServeHTTP(w, r)
 	})
 }
