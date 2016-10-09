4ea4b057115aafdd2578f6b0d60ab50ffd12fdc4
Gert Cuykens <gert.cuykens@gmail.com>
2016-10-09 11:37:33 +0200

    root

--- root.go
+++ root.go
@@ -8,6 +8,10 @@ import (
 // RootHTTP Handler
 func RootHTTP(root string, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			h.ServeHTTP(w, r)
+			return
+		}
 		if _, err := os.Stat(root + r.URL.Path); os.IsNotExist(err) {
 			h.ServeHTTP(w, r)
 			return
